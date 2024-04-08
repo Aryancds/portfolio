6bd24b6f880c201dcfae065eb8b1ddc71b65f130
Your Name <your_email@example.com>
2024-04-09 00:30:27 +0530

    new

--- src/comopnents/Hero.jsx
+++ src/comopnents/Hero.jsx
@@ -56,30 +56,30 @@ const Hero = () => {
 
                     <AnimatedDiv />
 
-                    <div className='row-span-3 text-center font-mono  '>
+                    <div className='row-span-3 text-center font-mono'>
 
-                        <div justify-center items-center>
+                        <div className='justify-center items-center'>
 
                             <div className='text-2xl mt-4'> Aryan Patel</div>
 
-                            <div className=' justify-center items-center space-y-2 grid grid-rows-2'>
+                            <div className='justify-center items-center space-y-2 grid grid-rows-2'>
 
-                                <div className='rounded justify-center items-center bg-stone-400 '>
+                                <div className=' justify-center items-center bg-stone-400  rounded-xl transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105'>
                                     Web Developer
                                 </div>
 
-                                <div className='flex justify-center items-center space-x-2 '>
-                                    <a href="https://www.facebook.com/profile.php?id=100036269282981&mibextid=rS40aB7S9Ucbxw6v" >
-                                        <div className='bg-stone-400 p-2 rounded-xl'><FaFacebook /></div>
+                                <div className='flex justify-center items-center space-x-2'>
+                                    <a href="https://www.facebook.com/profile.php?id=100036269282981&mibextid=rS40aB7S9Ucbxw6v">
+                                        <div className='bg-stone-400 p-2 rounded-xl transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105'><FaFacebook /></div>
                                     </a>
                                     <a href="https://twitter.com">
-                                        <div className='bg-stone-400 p-2 rounded-xl'><FaTwitter /></div>
+                                        <div className='bg-stone-400 p-2 rounded-xl transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105'><FaTwitter /></div>
                                     </a>
                                     <a href="https://www.linkedin.com/in/aryan-patel-a36196244/">
-                                        <div className='bg-stone-400 p-2 rounded-xl'><FaLinkedin /></div>
+                                        <div className='bg-stone-400 p-2 rounded-xl transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105'><FaLinkedin /></div>
                                     </a>
-                                    <a href="https://github.com/aryancds" >
-                                        <div className='bg-stone-400 p-2 rounded-xl'><FaGithub /></div>
+                                    <a href="https://github.com/aryancds">
+                                        <div className='bg-stone-400 p-2 rounded-xl transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105'><FaGithub /></div>
                                     </a>
                                 </div>
 
@@ -89,22 +89,23 @@ const Hero = () => {
 
                     </div>
 
+
                     <div className='row-span-4 font-mono h-full'>
 
                         <div className='grid grid-row-4 justify-center items-center space-y-4'>
-                            <div className="flex row-span-1 items-center bg-stone-400 rounded-xl p-2">
+                            <div className="flex row-span-1 items-center bg-stone-400 rounded-xl p-2 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
                                 <FaPhone className="text-xl" />
                                 <div className="ml-2">9484534949</div>
                             </div>
-                            <div className="flex row-span-1 items-center bg-stone-400 rounded-xl p-2">
+                            <div className="flex row-span-1 items-center bg-stone-400 rounded-xl p-2 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
                                 <FaEnvelope className="text-xl" />
                                 <div className="ml-2">art3miz07@gmail.com</div>
                             </div>
-                            <div className="flex row-span-1 items-center bg-stone-400 rounded-xl p-2">
+                            <div className="flex row-span-1 items-center bg-stone-400 rounded-xl p-2 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
                                 <FaMapMarkerAlt className="text-xl" />
                                 <div className="ml-2">Surat, Gujarat</div>
                             </div>
-                            <div className="flex row-span-1 items-center bg-stone-400 rounded-xl p-2">
+                            <div className="flex row-span-1 items-center bg-stone-400 rounded-xl p-2 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
                                 <FaCalendarAlt className="text-xl" />
                                 <div className="ml-2">18/08/2003</div>
                             </div>
@@ -112,14 +113,16 @@ const Hero = () => {
 
                     </div>
 
+
                     <div className='row-span-1 flex justify-center items-center'>
                         <a href="https://drive.google.com/file/d/1qwmiYjRBDs7cTn4fB22bxiyudNUMkND2/view?usp=sharing" target="_blank" rel="noopener noreferrer">
-                            <button className="bg-stone-500 hover:bg-stone-700 text-white font-bold py-2 px-4 rounded">
+                            <button className="bg-stone-500 hover:bg-stone-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
                                 <IoMdDownload className="w-6 h-6 inline-block" />
                                 <span className='font-mono'>Download CV</span>
                             </button>
                         </a>
                     </div>
+
 
                 </div>
 
@@ -128,33 +131,33 @@ const Hero = () => {
             <div className='p-4 items-center rounded-lg col-span-8 '>
                 <div className='text-center'>
 
-                    <h1 className='text-2xl font-mono p-4 '>Projects</h1>
+                    <div className='items-center justify-center rounded-lg bg-gray-200'>
+                        <h1 className='text-3xl  font-mono p-4'>Projects</h1>
+                    </div>
 
-                    <div className="grid grid-cols-1 sm:grid-cols-4 gap-4">
 
-                        <div className="bg-gray-200 p-4 rounded-md h-full">
+                    <div className="grid grid-cols-1 sm:grid-cols-4 gap-4 mt-4">
+                        <div className="bg-gray-200 p-4 rounded-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 shadow-lg">
                             <h2 className="text-xl font-bold mb-2">Diamond Price Calculation Software</h2>
                             <p className="text-gray-600">Overview: The Diamond Price Calculation Software is a comprehensive tool designed for jewelers, gemologists, and diamond traders to accurately calculate the price of diamonds based on various factors such as carat weight, cut, color, clarity, and market trends.</p>
                         </div>
 
-                        <div className="bg-gray-200 p-4 rounded-md">
+                        <div className="bg-gray-200 p-4 rounded-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 shadow-lg">
                             <h2 className="text-xl font-bold mb-2">Project 2</h2>
                             <p className="text-gray-600">Details about Project 2</p>
                         </div>
 
-
-                        <div className="bg-gray-200 p-4 rounded-md">
+                        <div className="bg-gray-200 p-4 rounded-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 shadow-lg">
                             <h2 className="text-xl font-bold mb-2">Project 3</h2>
                             <p className="text-gray-600">Details about Project 3</p>
                         </div>
 
-
-                        <div className="bg-gray-200 p-4 rounded-md">
+                        <div className="bg-gray-200 p-4 rounded-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 shadow-lg">
                             <h2 className="text-xl font-bold mb-2">Project 4</h2>
                             <p className="text-gray-600">Details about Project 4</p>
                         </div>
-
                     </div>
+
 
                 </div>
             </div>
