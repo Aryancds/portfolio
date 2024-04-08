e64d8414cc0da6b65464f6dc884c00b3dafcc8a0
Your Name <your_email@example.com>
2024-04-08 23:38:39 +0530

    background

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 
 import './App.css'
 import Hero from './comopnents/Hero'
-import backgroundImage from '/src/assets/bgmain.png'
 import { Analytics } from "@vercel/analytics/react"
+import backgroundImage from '/Gns38.png'
+
 
 
 
@@ -10,19 +11,20 @@ function App() {
 
   return (
     <>
-      <div className='p-4 h-full' >
+      <div className='p-4 h-full' style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        minHeight: '100vh',
+        minWidth: '100vw' // Ensures the image covers the entire viewport
+      }} >
 
         <Hero />
 
         <Analytics />
       </div>
     </>
-    // style={{
-    //   backgroundImage: `url(${backgroundImage})`,
-    //   backgroundSize: '',
-    //   backgroundPosition: 'center',
-    //   minHeight: '100vh', // Ensures the image covers the entire viewport
-    // }}
+
   )
 }
 
--- src/comopnents/Hero.jsx
+++ src/comopnents/Hero.jsx
@@ -7,6 +7,10 @@ import { gsap } from 'gsap';
 
 
 
+
+
+
+
 const AnimatedDiv = () => {
     const bgImageRef = useRef(null);
 
@@ -21,19 +25,32 @@ const AnimatedDiv = () => {
     return (
         <div className="p-12 rounded-sm sm:p-8 row-span-2" ref={bgImageRef}>
             <div className="p-2 bg-stone-400 rounded">
-                <img className="" src="/src/comopnents/bgmaina.jpg" alt="Example" />
+                <img className="" src="/bgmaina.jpg" alt="Example" />
             </div>
         </div>
+    );
+};
+
+
+
+
+const VideoBackground = () => {
+    return (
+        <video autoPlay muted loop className="absolute inset-0 z-0 w-full h-full object-cover">
+            <source src="your-video-file.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+        </video>
     );
 };
 
 const Hero = () => {
 
 
-    return (
-        <div className='mx-4 mt-20 sm:grid grid-cols-12 space-x-4 max-w-screen'>
 
-            <div className=' rounded-lg h-[620px]  bg-transperent col-span-2'>
+    return (
+        <div className='mx-4 mt-20 sm:grid grid-cols-12 space-x-4 max-w-screen' >
+
+            <div className=' rounded-lg h-[620px]  bg-transperent col-span-2' >
 
                 <div className='bg-white rounded-lg items-center justify-center h-full grid grid-rows-10'>
 
@@ -52,12 +69,18 @@ const Hero = () => {
                                 </div>
 
                                 <div className='flex justify-center items-center space-x-2 '>
-
-                                    <div className='bg-stone-400 p-2 rounded-xl'><FaFacebook /></div>
-                                    <div className='bg-stone-400 p-2 rounded-xl'><FaTwitter /></div>
-                                    <div className='bg-stone-400 p-2 rounded-xl'><FaLinkedin /></div>
-                                    <div className='bg-stone-400 p-2 rounded-xl'><FaGithub /></div>
-
+                                    <a href="https://www.facebook.com/profile.php?id=100036269282981&mibextid=rS40aB7S9Ucbxw6v" >
+                                        <div className='bg-stone-400 p-2 rounded-xl'><FaFacebook /></div>
+                                    </a>
+                                    <a href="https://twitter.com">
+                                        <div className='bg-stone-400 p-2 rounded-xl'><FaTwitter /></div>
+                                    </a>
+                                    <a href="https://www.linkedin.com/in/aryan-patel-a36196244/">
+                                        <div className='bg-stone-400 p-2 rounded-xl'><FaLinkedin /></div>
+                                    </a>
+                                    <a href="https://github.com/aryancds" >
+                                        <div className='bg-stone-400 p-2 rounded-xl'><FaGithub /></div>
+                                    </a>
                                 </div>
 
                             </div>
@@ -90,10 +113,12 @@ const Hero = () => {
                     </div>
 
                     <div className='row-span-1 flex justify-center items-center'>
-                        <button className="bg-stone-500 hover:bg-stone-700 text-white font-bold py-2 px-4 rounded">
-                            <IoMdDownload className="w-6 h-6 inline-block" />
-                            <span className='font-mono'>Download CV</span>
-                        </button>
+                        <a href="https://drive.google.com/file/d/1qwmiYjRBDs7cTn4fB22bxiyudNUMkND2/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                            <button className="bg-stone-500 hover:bg-stone-700 text-white font-bold py-2 px-4 rounded">
+                                <IoMdDownload className="w-6 h-6 inline-block" />
+                                <span className='font-mono'>Download CV</span>
+                            </button>
+                        </a>
                     </div>
 
                 </div>
@@ -105,29 +130,29 @@ const Hero = () => {
 
                     <h1 className='text-2xl font-mono p-4 '>Projects</h1>
 
-                    <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+                    <div className="grid grid-cols-1 sm:grid-cols-4 gap-4">
 
                         <div className="bg-gray-200 p-4 rounded-md h-full">
                             <h2 className="text-xl font-bold mb-2">Diamond Price Calculation Software</h2>
                             <p className="text-gray-600">Overview: The Diamond Price Calculation Software is a comprehensive tool designed for jewelers, gemologists, and diamond traders to accurately calculate the price of diamonds based on various factors such as carat weight, cut, color, clarity, and market trends.</p>
                         </div>
 
-                        {/* <div className="bg-gray-200 p-4 rounded-md">
+                        <div className="bg-gray-200 p-4 rounded-md">
                             <h2 className="text-xl font-bold mb-2">Project 2</h2>
                             <p className="text-gray-600">Details about Project 2</p>
                         </div>
 
-                        
+
                         <div className="bg-gray-200 p-4 rounded-md">
                             <h2 className="text-xl font-bold mb-2">Project 3</h2>
                             <p className="text-gray-600">Details about Project 3</p>
                         </div>
 
-                        
+
                         <div className="bg-gray-200 p-4 rounded-md">
                             <h2 className="text-xl font-bold mb-2">Project 4</h2>
                             <p className="text-gray-600">Details about Project 4</p>
-                        </div>  */}
+                        </div>
 
                     </div>
 
