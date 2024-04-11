100b7daff308555e3ab194d90bf1c4484738b43d
Aryan Patel <aryanpatel8877@gmail.com>
2024-04-11 14:36:18 +0530

    new

--- src/comopnents/Hero.jsx
+++ src/comopnents/Hero.jsx
@@ -161,6 +161,18 @@ const Hero = () => {
 
                 <div>
 
+                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7140779895140124"
+                        crossorigin="anonymous"></script>
+
+                    <ins class="adsbygoogle"
+                        style="display:block"
+                        data-ad-client="ca-pub-7140779895140124"
+                        data-ad-slot="6057064235"
+                        data-ad-format="auto"
+                        data-full-width-responsive="true"></ins>
+                    <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({ });
+                    </script>
                 </div>
 
             </div>
